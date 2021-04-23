171905684f42f9850b2d861355980d26925f1dab
Sergey Sotnikov <ssotnikov1990@gmail.com>
2021-04-23 13:48:36 +0300

    animated burger and hover icon

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu-link'),
+    cart = document.querySelector('.cart'),
     hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger-active');
         menu.classList.toggle('menu-active');
+        cart.classList.toggle('cart-menu-hover');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger-active');
             menu.classList.toggle('menu-active');
+            cart.classList.toggle('cart-menu-hover');
         })
     })
 })
